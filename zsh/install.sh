fcf28bed3fd01db29e99c95ac6f64afc4f40a228
leachim <leachim@users.noreply.github.com>
2023-09-20 20:58:40 +0100

    mu

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,6 @@
 # Check for Oh My Zsh and install if we don't have it
 if test ! $(which omz); then
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+  git clone --depth 1 --recurse-submodules https://github.com/ohmyzsh/ohmyzsh /home/$USER/.ohmyzsh
+
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
