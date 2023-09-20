b0abf21c64a9770fad20a07a4a1ce50b19b6d11e
leachim <leachim@users.noreply.github.com>
2023-09-20 22:36:05 +0100

    mu

--- vim/install.sh
+++ vim/install.sh
@@ -2,13 +2,23 @@
 
 ## vim
 echo "Installing Vim Plugins .."
-# install plug.vim for vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+command -v nvim >/dev/null
+
+if [[ $? -ne 0 ]]; then
+    echo "Nvim is not installed"
+  # install plug.vim for vim
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  vim +PlugInstall
+else
+    echo "Nvim is installed"
+  # and for nvim
+  #curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  #  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# and for nvim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#vim +PlugInstall
+  nvim +PlugInstall
+fi
 
 if test ! "$(uname)" = "Darwin"
   then
