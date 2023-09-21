d51ab3b7ec3576879957b103e2e0bf79a620ca88
leachim <leachim@users.noreply.github.com>
2023-09-21 09:02:36 +0100

    mu

--- vim/install.sh
+++ vim/install.sh
@@ -3,22 +3,23 @@
 ## vim
 echo "Installing Vim Plugins .."
 
-if ! command -v nvim &> /dev/null
+if command -v vim &> /dev/null
 then
     echo "Nvim is not installed"
   # install plug.vim for vim
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  vim -u "~/.dotfiles.vim/vimrc.symlink" +PlugInstall
+  vim -u "~/.dotfiles/vim/vimrc.symlink" +PlugInstall
 fi
-if
+
+if command -v nvim &> /dev/null
 echo "Nvim is installed"
   # and for nvim
   #curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
   #  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   sh -c 'curl -fLo "~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-  nvim "~/.dotfiles.vim/vimrc.symlink" +PlugInstall
+  nvim "~/.dotfiles/vim/vimrc.symlink" +PlugInstall
 fi
 
 if test ! "$(uname)" = "Darwin"
