00a66c0290927ade02fe52d0d9d80a44538d5fcf
leachim <>
2024-08-22 12:43:01 +0200

    fix issues on euler

--- starship/install.sh
+++ starship/install.sh
@@ -7,7 +7,7 @@ then
   brew install starship
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
 then
-  curl -sS https://starship.rs/install.sh | sh -s -- -y --bin-dir /home/$USER/.dotfiles/bin
+  curl -sS https://starship.rs/install.sh | sh -s -- -y --bin-dir /$HOME/.dotfiles/bin
 fi
 
 exit 0
