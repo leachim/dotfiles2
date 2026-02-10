4f0d84856b2f93290a1338d23419ebf6869d9337
leachim <leachim@users.noreply.github.com>
2026-02-10 16:45:51 +0100

    mu

--- hosts/hostnames/bs.sh
+++ hosts/hostnames/bs.sh
@@ -8,6 +8,8 @@ export CUDA_HOME=$HOME/cuda-12.8
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 
+# MMSEQ
+export PATH=/home/michaes/mmseqs/bin:$PATH
 # GCC 12 environment (specific to bs-* nodes)
 export PATH=$HOME/gcc-12/bin:$PATH
 export CC=$HOME/gcc-12/bin/gcc
@@ -27,4 +29,4 @@ export HTTPS_PROXY=$https_proxy
 # squeue aliases (different from qqsqu in hpc.sh - note 'squ' vs 'qqsqu')
 alias squ="squeue -u \$(whoami) -o \"%.18i %.12P %.20j %.3T %.12M %.10l %.6D %.4C %R\""
 alias sqw="watch -n 10 'squeue -u \$(whoami) -o \"%.18i %.12P %.20j %.3T %.12M %.10l %.6D %.4C %R\"'"
-alias sqwa="watch -n 10 'squeue -o \"%.18i %.12P %.20j %.8u %.3T %.12M %.10l %.6D %.4C %.16b %R\" | sed \"1!s/gres\/gpu:[^:]*://g; 1!s/N\/A/0/g\"'"
+alias sqwa="watch -n 10 'squeue -o \"%.18i %.12P %.20j %.8u %.3T %.12M %.10l %.6D %.4C %b %R\" | awk \"{if (NR==1) {print; next} if (\\\$10 ~ /gpu:/) {match(\\\$10, /[0-9]+\\\$/, a); \\\$10=a[0]} else if (\\\$10 ~ /N\/A/) {\\\$10=0} print}\" | column -t'"
